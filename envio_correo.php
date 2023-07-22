caf18f7d367ebb560bdfe832d797ce29d0f97945
Coyocon <leroz_@hotmail.com>
2023-07-22 08:54:06 -0600

    codigo prueba

--- envio_correo.php
+++ envio_correo.php
@@ -2,13 +2,22 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+
+session_start();
+$method=$_SERVER['REQUEST_METHOD'];
+if($method == 'POST'){
+    if(isset($_POST['codigo'])){
+        if($_SESSION['code']==$_POST['codigo'])
+        echo 'Codigo Correcto <br><hr>';
+
+    }else{
+
 require 'assest/PHPMailer/Exception.php';
 require 'assest/PHPMailer/PHPMailer.php';
 require 'assest/PHPMailer/SMTP.php';
 $mail = new PHPMailer(true);
 
 try{
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();
     $mail->Host = 'smtp.hostinger.com';
     $mail->SMTPAuth = true;
@@ -18,18 +27,30 @@ try{
     $mail->Port = 465;
 
     $mail->setFrom('pruebas@development.cescoyoc.dev','App en Desarrollo');
-    $mail->addAddress('cesarcoyoc@gmail.com');
+    $mail->addAddress($_POST['email']);
     $mail->isHTML(true);
     $code='';
     for ($i=0; $i < 6 ; $i++) { 
         $code.=rand(0,9);
     }
+    $_SESSION['code']=$code;
     $mail->Subject = 'Codigo de acceso a cuenta';
     $mail->Body="Tu codigo de acceso es <b>$code</b>";
     $mail->send();
-    echo 'Correo enviado';
-    echo 'Codigo '.$code;
 } catch(Exception $e){
     echo 'Mensaje'. $mail->ErrorInfo;
 }
+    }
+}
 ?>
+<form action="" method="POST">
+<?php if(isset($code)) { ?>
+    <input name="codigo" type="text">
+    <button> Verificar codigo </button>
+<?php }else{ ?>
+
+    <input name="email" type="Mail">
+    <button> Enviar codigo </button>
+
+<?php }?>
+</form>
