6faf124b8b504e676f165a577046642b252a11e6
Coyocon <leroz_@hotmail.com>
2023-07-25 12:49:04 -0600

    Base principal

--- views/default/menu.php
+++ views/default/menu.php
@@ -1,30 +1,32 @@
-<nav id="navbar-puryfiel" class="navbar navbar-expand-sm  background-puryfiel" >
-    <a class="navbar-brand " href="#"> 
-    
-    </a>
-    <button class="navbar-toggler me-2 border-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end background-puryfiel" style="width:70%" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body ">
-            <ul class="navbar-nav justify-content-center flex-fill">
-                <li class="nav-item">
-                    <a class="nav-link mx-0 mx-sm-1 link-puryfiel" aria-current="page" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mx-0 mx-sm-1 link-puryfiel" href="#Seccion2">Ruta</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mx-0 mx-sm-1 link-puryfiel" href="#Seccion3">Acerca</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mx-0 mx-sm-1 link-puryfiel" href="#Seccion4">Contacto</a>
-                </li>
-            </ul>
+<nav id="navbar-puryfiel" class="navbar navbar-expand-sm fixed-top background-gym" >
+    <div class="container-fluid">
+        <a class="navbar-brand " href="#"> 
+        
+        </a>
+        <button class="navbar-toggler me-2 border-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end background-gym" style="width:70%" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body ">
+                <ul class="navbar-nav justify-content-center flex-fill">
+                    <li class="nav-item">
+                        <a class="nav-link mx-0 mx-sm-1 link-gym" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-0 mx-sm-1 link-gym" href="#Seccion2">Ruta</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-0 mx-sm-1 link-gym" href="#Seccion3">Acerca</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-0 mx-sm-1 link-gym" href="#Seccion4">Contacto</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
--- views/pages/home.php
+++ views/pages/home.php
@@ -1,3 +1,126 @@
-<div class="row bg-success" style="margin-top:50px;">
+<div class="row" style="margin-top:50px;">
+    <div class="position-fixed">
+        <section>
+            <div id="seccion1" class="row seccion pt-3">
+                <div id="carousel_puryfiel" class="carousel slide carousel-fade carrusel px-0 mx-0">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carousel_puryfiel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carousel_puryfiel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwzfHxjb21wYW55JTJDJTIwd2F0ZXJ8ZW58MHx8fHwxNjg5MDk2Njg1fDA&ixlib=rb-4.0.3&auto=format&fit=crop&w=812&h=344" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwxfHxjb21wYW55JTJDJTIwcXVhbGl0eXxlbnwwfHx8fDE2ODkwNzQ3NjN8MA&ixlib=rb-4.0.3&auto=format&fit=crop&w=612&h=544" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block ">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel_puryfiel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon color-puryfiel" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel_puryfiel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="z-1" style="margin-top: 60vh;">
+        <section>
+            <div id="seccion2" class="row seccion ">
+                <div class="col-6 px-0">
+                    <div class="col-12">
+                    <img src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwzfHxjb21wYW55JTJDJTIwd2F0ZXJ8ZW58MHx8fHwxNjg5MDk2Njg1fDA&ixlib=rb-4.0.3&auto=format&fit=crop&w=812&h=344" class="d-block w-100 img-cover" alt="...">
+                    </div>
+                </div>
+                <div class="col-6 px-0">
+                    <div class="col-12">
+                    <img src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwzfHxjb21wYW55JTJDJTIwd2F0ZXJ8ZW58MHx8fHwxNjg5MDk2Njg1fDA&ixlib=rb-4.0.3&auto=format&fit=crop&w=812&h=344" class="d-block w-100 img-cover" alt="...">
 
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div id="seccion3" class="row seccion bg-danger d-flex justify-content-evenly py-3">
+                <div class="col-2 text-white">
+                    <div class="row text-center mt-3">
+                        <h5>Entranamiento</h5>
+                    </div>
+                    <div class="row bg-danger justify-content-center my-3">
+                        <div class="col-8" >
+                            <img style="height:15vh;" class="img-cover rounded-circle img-fluid" src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwzfHxjb21wYW55JTJDJTIwd2F0ZXJ8ZW58MHx8fHwxNjg5MDk2Njg1fDA&ixlib=rb-4.0.3&auto=format&fit=crop&w=812&h=344" alt="">
+                        </div>
+                    </div>
+                    <div class="row text-center mb-3">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, fugit.</p>
+                    </div>
+                </div>
+                <div class="col-2 text-white">
+                    <div class="row text-center mt-3">
+                        <h5>Entranamiento</h5>
+                    </div>
+                    <div class="row bg-danger justify-content-center my-3">
+                        <div class="col-8" >
+                            <img style="height:15vh;" class="img-cover rounded-circle img-fluid" src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwzfHxjb21wYW55JTJDJTIwd2F0ZXJ8ZW58MHx8fHwxNjg5MDk2Njg1fDA&ixlib=rb-4.0.3&auto=format&fit=crop&w=812&h=344" alt="">
+                        </div>
+                    </div>
+                    <div class="row text-center mb-3">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, fugit.</p>
+                    </div>
+                </div>
+                <div class="col-2 text-white">
+                    <div class="row text-center mt-3">
+                        <h5>Entranamiento</h5>
+                    </div>
+                    <div class="row bg-danger justify-content-center my-3">
+                        <div class="col-8" >
+                            <img style="height:15vh;" class="img-cover rounded-circle img-fluid" src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwzfHxjb21wYW55JTJDJTIwd2F0ZXJ8ZW58MHx8fHwxNjg5MDk2Njg1fDA&ixlib=rb-4.0.3&auto=format&fit=crop&w=812&h=344" alt="">
+                        </div>
+                    </div>
+                    <div class="row text-center mb-3">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, fugit.</p>
+                    </div>
+                </div>
+                <div class="col-2 text-white">
+                    <div class="row text-center mt-3">
+                        <h5>Entranamiento</h5>
+                    </div>
+                    <div class="row bg-danger justify-content-center my-3">
+                        <div class="col-8" >
+                            <img style="height:15vh;" class="img-cover rounded-circle img-fluid" src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwzfHxjb21wYW55JTJDJTIwd2F0ZXJ8ZW58MHx8fHwxNjg5MDk2Njg1fDA&ixlib=rb-4.0.3&auto=format&fit=crop&w=812&h=344" alt="">
+                        </div>
+                    </div>
+                    <div class="row text-center mb-3">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, fugit.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div id="seccion2" class="row seccion ">
+                <div class="col-6 px-0">
+                    <div class="col-12">
+                    <img src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwzfHxjb21wYW55JTJDJTIwd2F0ZXJ8ZW58MHx8fHwxNjg5MDk2Njg1fDA&ixlib=rb-4.0.3&auto=format&fit=crop&w=812&h=344" class="d-block w-100 img-cover" alt="...">
+                    </div>
+                </div>
+                <div class="col-6 px-0">
+                    <div class="col-12">
+                    <img src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?ixid=M3wzOTE5Mjl8MHwxfHNlYXJjaHwzfHxjb21wYW55JTJDJTIwd2F0ZXJ8ZW58MHx8fHwxNjg5MDk2Njg1fDA&ixlib=rb-4.0.3&auto=format&fit=crop&w=812&h=344" class="d-block w-100 img-cover" alt="...">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </div>
