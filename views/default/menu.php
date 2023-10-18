6a8d93820ea89864d920df1771640596f8555a73
Coyocon <leroz_@hotmail.com>
2023-10-18 17:44:41 -0600

    Uso de datos en home

--- views/default/menu.php
+++ views/default/menu.php
@@ -16,10 +16,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="componentes/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?=$_SESSION['imagen']?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block"><?=ucfirst($_SESSION['name'])." ".ucfirst($_SESSION['apell']) ?></a>
         </div>
       </div>
 
