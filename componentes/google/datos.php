32c4b2c34dab79742e8a8c05bc3af385951498c9
Coyocon <leroz_@hotmail.com>
2023-10-20 09:30:29 -0600

    cambios

--- componentes/google/datos.php
+++ componentes/google/datos.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
+session_start();
 $clienteID ="283601058959-giep9gmlmdjouq4ic5dotk0lns0ckocn.apps.googleusercontent.com";
 $clienteS="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";
-$url_redirecc="http://localhost/gym_boutique/";
-DEFINE('URL','http://'.$_SERVER['HTTP_HOST'].'/gym_boutique/home');
+$url_redirecc="https://".$_SERVER['HTTP_HOST']."/";
+DEFINE('URL','https://'.$_SERVER['HTTP_HOST'].'/home');
 ?>
