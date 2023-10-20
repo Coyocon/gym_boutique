0dc10a3cd3bac41993404501ffb00ee7d10a0b68
Coyocon <leroz_@hotmail.com>
2023-10-20 09:41:19 -0600

    correciones

--- componentes/google/datos.php
+++ componentes/google/datos.php
@@ -1,7 +1,6 @@
 <?php
-session_start();
 $clienteID ="283601058959-giep9gmlmdjouq4ic5dotk0lns0ckocn.apps.googleusercontent.com";
 $clienteS="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";
-$url_redirecc="https://".$_SERVER['HTTP_HOST']."/";
-DEFINE('URL','https://'.$_SERVER['HTTP_HOST'].'/home');
+$url_redirecc="http://".$_SERVER['HTTP_HOST']."/gym_boutique/";
+DEFINE('URL','http://'.$_SERVER['HTTP_HOST'].'/gym_boutique/home');
 ?>
--- views/pages/login.php
+++ views/pages/login.php
@@ -1,27 +1,7 @@
 <?php
 require_once 'vendor/autoload.php';
-require_once 'componentes/config/loginGoogle.php';
-$google_client = new Google\Client();
-$google_client -> setClientId($clienteID);
-$google_client -> setClientSecret($clienteS);
-$google_client -> setRedirectUri($url_redirecc);
-$google_client ->addScope("email");
-$google_client -> addScope("profile");
+require_once 'componentes/google/loginGoogle.php';
 
-if (isset($_GET['code'])){
-    $token = $google_client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $google_client ->setAccessToken($token['access_token']);
-    $google_oauth = New Google_Service_Oauth2($google_client);
-    $google_account_info=$google_oauth->userinfo->get();
-    $datos['name']= $google_account_info->givenName;
-    $datos['apell']= $google_account_info->familyName;
-    $datos['gener']= $google_account_info->gender;
-    $datos['email'] = $google_account_info->email;
-    $datos['imagen']=$google_account_info->picture;
-    session_start();
-    $_SESSION=$datos;
-    header("location:".URL);
-}
 ?>
 <div class="login-box">
   <div class="login-logo">
