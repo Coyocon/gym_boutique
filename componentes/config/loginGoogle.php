af030dc685270ed7d081e6c612624c7bd37621c3
Coyocon <leroz_@hotmail.com>
2023-10-20 09:30:29 -0600

    cambios

--- componentes/config/loginGoogle.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$clienteID ="283601058959-giep9gmlmdjouq4ic5dotk0lns0ckocn.apps.googleusercontent.com";
-$clienteS="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";
-$url_redirecc="https://".$_SERVER['HTTP_HOST']."/";
-DEFINE('URL','https://'.$_SERVER['HTTP_HOST'].'/home');
-?>
